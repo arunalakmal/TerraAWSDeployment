8cf48ba5d23035ec4b13d421e10952e8e02bd9b0
Aruna Lakmal <arunarandunu@gmail.com>
2019-10-14 10:27:16 +0000

    vpc configuration part 1

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,3 @@
 aws_profile = "superhero"
 aws_region  = "us-east-1"
+vpc_cidr    = "10.0.0.0/16"
--- variables.tf
+++ variables.tf
@@ -1,2 +1,4 @@
 variable "aws_region" {}
 variable "aws_profile" {}
+data "aws_availability_zones" "available" {}
+variable "vpc_cidr" {}
