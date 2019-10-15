7b56a21e6764bf65ccda86438c0f7c383a7471a8
Aruna Lakmal <arunarandunu@gmail.com>
2019-10-15 09:40:39 +0530

    RDS Subnet Group Added

--- main.tf
+++ main.tf
@@ -175,3 +175,16 @@ resource "aws_subnet" "wp_rds3_subnet" {
     Name = "wp_rds3"
   }
 }
+
+#RDS subnet group
+
+resource "aws_db_subnet_group" "wp_rds_subnetgroup" {
+  name = "wp_rds_subnetgroup"
+
+  subnet_ids = ["${aws_subnet.wp_rds1_subnet.id}",
+                "${aws_subnet.wp_rds2_subnet.id}",
+                "${aws_subnet.wp_rds3_subnet.id}"]
+  tags = {
+    Name = "wp_rds_sng"
+  }
+}
