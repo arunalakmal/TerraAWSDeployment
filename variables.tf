99559944ea081d02501b2be0c68b0376d7e1c878
Aruna Lakmal <aruna.lakmal@pearson.com>
2019-10-16 20:00:26 +0530

    ELB added

--- variables.tf
+++ variables.tf
@@ -17,3 +17,10 @@ variable "dev_instance_type" {}
 variable "dev_ami" {}
 variable "public_key_path" {}
 variable "key_name" {}
+variable "elb_healthy_threshold" {}
+variable "elb_unhealthy_threshold" {}
+variable "elb_timeout" {}
+variable "elb_interval" {}
+
+
+
