c7fe0fbf808fae5d4a130456e94276f4ec445ed6
Aruna Lakmal <aruna.lakmal@pearson.com>
2019-10-16 21:39:12 +0530

    AMI Created

--- main.tf
+++ main.tf
@@ -438,3 +438,27 @@ resource "aws_elb" "wp_elb" {
     Name = "wp_${var.domain_name}-elb"
   }
 }
+
+#------ Golden AMI -----
+# random ami id 
+resource "random_id" "golden_ami" {
+  byte_length = 3
+}
+
+# Create AMI
+
+resource "aws_ami_from_instance" "wp_golden" {
+  name               = "wp_ami-${random_id.golden_ami.b64}"
+  source_instance_id = "${aws_instance.wp_dev.id}"
+
+  provisioner "local-exec" {
+    command = <<EOT
+    cat <<EOF > userdata
+    #!/bin/bash
+    /usr/bin/aws s3 sync s3://${aws_s3_bucket.code.bucket} /var/www/html/
+    /bin/touch /var/spool/cron/root
+    sudo /bin/echo '*/5 * * * * aws s3 sync s3://${aws_s3_bucket.code.bucket} /var/www/html' >> /var/spool/cron/root
+    EOF
+    EOT
+  }
+}
